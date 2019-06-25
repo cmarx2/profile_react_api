1253114db72ee8802c404c8fa3f2e14c9501c71d
Candice Marx <candice.marx@hotmail.com>
2019-06-24 20:36:20 -0600

    profile using react context api

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Navbar from './components/shared/Navbar';
+import AccountProfile from './components/account/AccountProfile';
+
+const App = () => (
+  <>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route exact path='/' render={ () => <div>Home</div> } />
+        <Route exact path='/account/profile' component={AccountProfile} />
+      </Switch>
+    </Container>
+  </>
+)
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import AccountProvider from './providers/AccountProvider';
+
+ReactDOM.render(
+  <AccountProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter> 
+  </AccountProvider>,
+  document.getElementById('root')
+);
